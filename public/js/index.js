5faa9c325794afec035d1e6f405d717a83758388
daniel <xiaoaoxtu@gmail.com>
2017-04-12 20:11:13 +0800

    add front page pagination,dropload refresh feture

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,105 @@
+var currentPage = 0;
 $(document).ready(function(){
-    if("geolocation" in navigator){
-        navigator.geolocation.getCurrentPosition(function(position) {
-            console.log(position.coords.latitude, position.coords.longitude);
-            start = '' + position.coords.latitude + ',' + position.coords.longitude;
-            // desturl = 'http://m.amap.com/navi/?start=' + start + '&dest=116.470098,39.992838&destName=阜通西&key=85eb92c0e8b2f661e9fce2b29e0175c1';
-            // posurl = 'http://m.amap.com/navi/?dest=116.470098,39.992838&destName=阜通西&key=85eb92c0e8b2f661e9fce2b29e0175c1';
-            posurl = 'http://m.amap.com/navi/?dest=' + start + '&destName=阜通西&key=85eb92c0e8b2f661e9fce2b29e0175c1';
+    // var currentLocation = {latitude: 40.743827, longitude: -73.989015};
+    // var sortedData = getStarbuckList(function(data){
+    //     newdata = data.map(function(ele){
+    //         var distanceToCurrentLocation = distance(currentLocation, ele.location);
+    //         ele["distance"] = distanceToCurrentLocation;
+    //         return ele;
+    //     });
+    //     newdata.sort(function(elea, eleb){
+    //         return elea.distance - eleb.distance;
+    //     });
 
-            desturl = 'http://m.amap.com/navi/?start=116.403124,39.940693&dest=116.481488,39.990464&destName=一条驾车路线&key=85eb92c0e8b2f661e9fce2b29e0175c1';
-            console.log(posurl);
-            window.open(posurl);
-        });
-    }
+    //     newdata.forEach(function(data){
+    //         var starbuckCell = new Starbuck(data);
+    //         starbuckCell.appendTo("body");
+    //     });
+
+    // });
+    // getStarbuckList(1, function(data){
+    //     setData(data);
+    // });
+    setDropLoad();
 });
-
-function getStarbuckList(callback){
+function setData(data){
+    data.forEach(function(ele){
+        var starbuckCell = new Starbuck(ele);
+        starbuckCell.appendTo("#maincontainer");
+    });
+}
+function setDropLoad()
+{
+    $("#maincontainer").dropload({
+        scrollArea: window,
+        loadDownFn: function(me){
+            getStarbuckList(currentPage + 1, function(data){
+                setData(data);
+                me.resetload();
+            });
+        },
+        loadUpFn: function(me){
+            me.resetload();
+        }
+    });
+}
+function getStarbuckList(page, callback){
     $.ajax({
         url: "http://localhost:3000/starbucks",
         type: "GET",
+        data: {"page":page},
         contentType: "application/x-www-form-urlencoded; charset=utf-8",
         dataType: "json",
         success: function(data){
+            if(data.length == 0){
+                return;
+            }
             callback(data);
+            currentPage = page;
         }
     });
 }
 
 function Starbuck(data){
+    var starId = "starbuck-" + data.id;
+    var starbuckCell = $('<div>').attr("id", starId).addClass("starbuck-cell");
+    var starbuckImg = $("<a>").addClass("fleft").appendTo(starbuckCell);
+    $("<img>").addClass("starbuck-logo").attr("src","images/starbucks-logo.png").appendTo(starbuckImg);
+    var storeMessage = $("<div>").addClass("fleft").addClass("storemessage").appendTo(starbuckCell);
+    var starname = $("<div>").appendTo(storeMessage);
+    $("<h1>").addClass("storename").text(data.name).appendTo(starname);
 
+    var location = $("<div>").addClass("location").appendTo(storeMessage);
+    var address = data.street + '  ' + data.city;
+    //map address
+    var mapaddress = 'http://m.amap.com/navi/?dest='+data.location.latitude + ',' + data.location.longitude + '&destName=' + data.name + '&hideRouteIcon=1&key=85eb92c0e8b2f661e9fce2b29e0175c1';
+    var addressView = $("<a>").text(address).attr("href",mapaddress).appendTo(location);
+    $("<img>").attr("src", "images/bg.png").appendTo(addressView);
+
+    $("<div>").addClass("clearfloat").addClass("divider").appendTo(starbuckCell);
+
+    return starbuckCell;
 }
+/**
+ * Caculate the distance between two coordinate
+ */
+
+function distance(coordinateX, coordinateY){
+    var lat1 = coordinateX.latitude;
+    var lng1 = coordinateX.longitude;
+    var lat2 = coordinateY.latitude;
+    var lng2 = coordinateY.longitude;
+    var radLat1 = lat1 * Math.PI / 180.0;
+    var radLat2 = lat2 * Math.PI / 180.0;
+    var a = radLat1 - radLat2;
+    var b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;
+    s = Math.round(s * 10000) / 10000;
+    return s;
+}
+
+
+
+
+
--- routes/starbucks.js
+++ routes/starbucks.js
@@ -4,9 +4,19 @@ var fs = require('fs');
 
 /* GET starbucks listing. */
 router.get('/', function(req, res, next) {
-    console.log(req.query.page);
+
+    var page = req.param("page", 1);
+    var perpage = req.param("perpage", 20);
+
     var obj = JSON.parse(fs.readFileSync('./public/json/starbucks_new_york.json','utf8'));
-    res.json(obj);
+    var totalPages = obj.length / perpage;
+    if(page > totalPages){
+        res.json([]);
+        return;
+    }
+    var start = (page - 1) * perpage;
+    var data = obj.slice(start,start + perpage);
+    res.json(data);
 });
 
 module.exports = router;
